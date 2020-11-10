41e1ffbcd905a13955334163ea24e80027dde59b
junpei0029 <junpei.k.29@gmail.com>
2020-11-10 14:48:50 +0000

    3章【画像投稿機能を作ろう】

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fc86137e3b8109c0fc3dc21349991705ed67d1d8b281afdb3bbf106d461c899a3d1b24a873c98ffd6ac50add70a5968245ec0ace5fe50c284ad2103776b8372f'
